7885d81bca43e223ba2121433c3fc2038e2ac41d
Car234 <car234@txstate.edu>
2016-11-29 01:12:17 -0600

    Update main.c

--- main.c
+++ main.c
@@ -3,18 +3,6 @@
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-int main(void)
-{
-    int count = 1;
-    setlocale( LC_ALL, "" );
-    bindtextdomain( "hello", "/usr/share/locale" );
-    textdomain( "hello" );
-    printf( gettext( "Hello, world!\n" ) );
-    printf( ngettext( "Orangutan has %d banana.\n", "Orangutan has %d bananas.\n", count ), count );
-    printf( gettext( "Try Weblate at <http://demo.weblate.org/>!\n" ) );
-    printf( "%s\n", gettext( "Thank you for using Weblate." ) );
-    exit(0);
-}
 
 void test_hello_world()
 {
